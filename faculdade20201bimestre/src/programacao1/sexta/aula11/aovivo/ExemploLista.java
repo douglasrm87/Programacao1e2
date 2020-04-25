d6fc9288774237c77b753ddfbd547e3ccab719de
Your Name <you@example.com>
2020-04-24 21:08:25 -0300

    lista com objetos

--- faculdade20201bimestre/src/programacao1/sexta/aula11/aovivo/ExemploLista.java
+++ faculdade20201bimestre/src/programacao1/sexta/aula11/aovivo/ExemploLista.java
@@ -1,38 +1,40 @@
 package programacao1.sexta.aula11.aovivo;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ExemploLista {
-	
+/*
+A partir do Java 5 ofi criado os Tipos Parametrizados = Generics
+ */
 	public static void main(String[] args) {
-		List lista; // apenas uma referencia
-		lista = new ArrayList(); // criamso um objeto
+		List <String> lista; // apenas uma referencia
+		lista = new ArrayList<>(); // criamso um objeto
 		lista.add("Douglas");
 		lista.add("Mendes");
 		lista.add("Rocha");
 		lista.add("João");
 		lista.add("Pedro");
 		lista.add("Eu");
-		lista.add(1);
-		lista.add(22);
-		lista.add(134);
+		lista.add("1");
+		lista.add("22");
+		lista.add("134");
 		for (int i = 0; i < lista.size(); i++) {
 			System.out.println(lista.get(i));
 		}
-		
-		List lista2 = new ArrayList();
+
+		List <Integer> lista2 = new LinkedList<>();
 		lista2.add(90);
 		lista2.add(30);
 		lista2.add(90);
 		double total = 0.0;
-		for (int i = 0; i < lista.size(); i++) {
-			total = total + (double) lista2.get(i);
+		for (int i = 0; i < lista2.size(); i++) {
+			total = total + (int) lista2.get(i);
+			System.out.println("total:" + total);
 		}
-		System.out.println("Media: " + total/lista2.size());
-		
-		
-		
+		System.out.println("Media: " + total / lista2.size());
+
 	}
 
 }
